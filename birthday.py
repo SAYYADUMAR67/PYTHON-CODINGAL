d11440266fa3e0b7f471bbc1b81acf5f04fb3eb9
SAYYADUMAR67 <sayyad67hashmi@gmail.com>
2026-07-17 15:34:33 +0530

    data types technologia

--- birthday.py
+++ birthday.py
@@ -1,8 +1,8 @@
-myself=26/july/2013
-print("myself")
-mother=2/january/1982
-print("mother")
-sister=26/october/2013
-print("sister")
-dad=11/march/1980
-print("dad")
+myself="26/july/2013"
+print(myself)
+mother="2/january/1982"
+print(mother)
+sister="26/october/2013"
+print(sister)
+dad="11/march/1980"
+print("dad",dad)
---
+++ data types.py
@@ -0,0 +1,8 @@
+u=67
+m=67.0
+a=True
+r="i am smart"
+print(type(u))
+print(type(m))
+print(type(a))
+print(type(r))
