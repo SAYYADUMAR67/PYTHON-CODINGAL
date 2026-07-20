9f66acd726b0265bb47b93af9bfa97900c9d2441
SAYYADUMAR67 <sayyad67hashmi@gmail.com>
2026-07-20 15:03:16 +0530

    uhhhhhhhhhhh

---
+++ congratulations.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+
+message = "Congratulations " + name.title() + "!"
+decorated_message = message.upper()
+
+print(decorated_message)
+print("We are proud of you, " + name)
