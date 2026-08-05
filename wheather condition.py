d6759350dd063fb5011b826eb5b9a046ecb419ee
SAYYADUMAR67 <sayyad67hashmi@gmail.com>
2026-08-05 14:01:54 +0400

    too confusing

---
+++ wheather condition.py
@@ -0,0 +1,8 @@
+def wheather_condition():
+    print("The weather is very pleasant in", spring)
+    print("The weather is also same in", autumn)
+
+spring = "autumn"
+autumn = spring
+
+wheather_condition()
