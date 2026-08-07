1c1bc6009df607eb224a052521302eab52b95587
SAYYADUMAR67 <sayyad67hashmi@gmail.com>
2026-08-07 14:31:55 +0400

    cw g32mtrw

---
+++ pass.py
@@ -0,0 +1,11 @@
+for x in range(1,21):
+    if x%20 == 0:
+        print("Plot twist")
+    elif x%15 == 0:
+        pass
+    elif x%5 == 0:
+            print("fizz coca cola")
+    elif x%3 == 0:
+            print("buzz pepsi")
+    else:
+        print(x)
