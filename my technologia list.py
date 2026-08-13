d0c07ea694875d80f65a8db667962864f6fd74ab
SAYYADUMAR67 <sayyad67hashmi@gmail.com>
2026-08-13 14:00:07 +0400

    hi

---
+++ my technologia list.py
@@ -0,0 +1,11 @@
+empty_list =[] 
+
+print ()
+number = [1,2,3,4,5,6,7,8,9,10,'technologia']
+print(number)
+triple = ['technologia']*3
+print(triple)
+
+alistic=[670,600,700,167,267]
+alistic.reverse()
+print(alistic)
