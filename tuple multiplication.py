90388c027032d70dbd1ebeea21f07222d74c9626
SAYYADUMAR67 <sayyad67hashmi@gmail.com>
2026-08-14 14:48:40 +0400

    gueifrifjr

---
+++ tuple multiplication.py
@@ -0,0 +1,8 @@
+import math
+ 
+nums = (4, 3, 2, 2, 1, 18,1,3,1,3,1,3,2,3,3,2,2,.32,5,3.2,6,7.6,87,76,43.3,3.4)
+ 
+result = math.prod(nums)
+ 
+print("Original Tuple:", nums)
+print("Product - multiplying all the numbers of the said tuple:", result)
