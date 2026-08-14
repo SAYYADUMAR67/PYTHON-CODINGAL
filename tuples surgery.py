e9f3e0091cdd970780aba9ac61666a284e023cd1
SAYYADUMAR67 <sayyad67hashmi@gmail.com>
2026-08-14 13:54:43 +0400

    gogo gaga

---
+++ tuples surgery.py
@@ -0,0 +1,20 @@
+tuplex = ("surgery",True, 2024, 6, 20, 15, 30,6.7)
+print(tuplex)
+
+tuple = (1,4,2,5,3,2,5,2,5,2)
+print(tuple)
+
+tuple = tuple + (9,)
+print(tuple)
+
+tuple1 = (1,2,3,4,51,1,1,1,1,1)
+print(tuple1)
+
+count = tuple1.count(1)
+print(count)
+
+tuple2 = (1,2,3,4,5,6,7,8,9)
+print(tuple2)
+
+slice = tuple2[2:5]
+print(slice)
