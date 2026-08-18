fb01e1acac5519ccad90db3e64787e78f1f1dc1d
SAYYADUMAR67 <sayyad67hashmi@gmail.com>
2026-08-18 13:54:02 +0400

    sigma kingdom

---
+++ sets operations.py
@@ -0,0 +1,11 @@
+my_set = {1,2,3,4,5,6,7,8,9,10}
+print (my_set)
+
+my_kingdom = {1,2,3,0.4,5,6.8,7.9,8,9,1.00}
+print (my_kingdom)
+
+my_sigma = {1,2,3,"umar",6,"is",8,"sigma",10}
+print (my_sigma)
+
+my_set = {1,2,3,4,5,6,7,6,7,1}
+print (my_set)
