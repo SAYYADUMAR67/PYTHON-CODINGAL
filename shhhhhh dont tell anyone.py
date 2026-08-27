e0e9cf077a51956c30b38cc543c9dd5ec448dbc5
SAYYADUMAR67 <sayyad67hashmi@gmail.com>
2026-08-27 14:05:09 +0400

    trgrrdredrgertrgff83mcngr9usahcnblkoiewk

---
+++ shhhhhh dont tell anyone.py
@@ -0,0 +1,11 @@
+class shhhhh:
+    __privatevar = 28;
+    def __privmeth(self):
+        print("sshhhhh this is my god method")
+    def hello(self):
+        print("private variable value",shhhhh.__privatevar)
+
+
+privates = shhhhh()
+privates.hello()
+privates.__privmeth
