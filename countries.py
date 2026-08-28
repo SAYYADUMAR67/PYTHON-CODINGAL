aaae868faadb130ba52ae8d346c4f0c6c7afd105
SAYYADUMAR67 <sayyad67hashmi@gmail.com>
2026-08-28 14:30:04 +0400

    eugj4krkogj

---
+++ countries.py
@@ -0,0 +1,20 @@
+class india():
+    def capital(self):
+        print("New Delhi is the capital of india.")
+
+    def language(self):
+        print("hindi is the mostly spoken language in india.")
+
+    def type(self):
+        print("india is a developing country")
+
+
+class usa():
+    def capital(self):
+        print("washington, D.C is the capital of usa.")
+
+    def language(self):
+        print("english is the mostly spoken language in usa.")
+
+    def type(self):
+        print("usa is a developed country")
